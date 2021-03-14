d97061e9b60601dd7a4ef487956deca4d3cc5568
Nanang Mahdaen El Agung <mahdaen@gmail.com>
2021-03-14 21:00:33 +0700

    Initial commit

---
+++ readme.md
@@ -0,0 +1,8 @@
+A simple promise based timeout.
+
+**Example**
+```typescript
+import { sleep } from '@stater/sleep';
+
+await sleep(1000);
+```
---
+++ src/index.ts
@@ -0,0 +1,20 @@
+/**
+ * A simple promise based setTimout.
+ * @example
+ * ```typescript
+ * // Using standard timeout.
+ * setTimeout(async() => {
+ *   await record.save();
+ * }, 3000);
+ *
+ * // Using sleep.
+ * await sleep(3000);
+ * await record.save();
+ * ```
+ * @param timeout - Sleep duration in milliseconds.
+ */
+export function sleep(timeout: number): Promise<void> {
+  return new Promise((resolve) => {
+    setTimeout(resolve, timeout);
+  });
+}
